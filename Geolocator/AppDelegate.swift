2663f8644bec0a817cd659ef7d41b0b70d021ad4
Emory Dunn <edunn@emorydunn.com>
2018-12-11 19:40:51 -0800

    Initial Commit

---
+++ Geolocator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Geolocator
+//
+//  Created by Emory Dunn on 11 December, 2018.
+//  Copyright © 2018 Emory Dunn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
