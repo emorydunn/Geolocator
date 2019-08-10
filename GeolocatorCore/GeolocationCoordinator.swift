a73ce240de484fb9e8676dca4ff66fbb581c9bd5
Emory Dunn <edunn@emorydunn.com>
2019-08-09 20:27:28 -0700

    Only read needed metadata

--- GeolocatorCore/GeolocationCoordinator.swift
+++ GeolocatorCore/GeolocationCoordinator.swift
@@ -23,7 +23,26 @@ public class GeolocationCoordinator {
         // Load the contents of the given URLs
         let fullContents = try ImageLoader.contents(of: urls)
         
-        try reader.read(from: fullContents)
+        let tags = [
+            // Basic Info
+            TagGroup.File.FileName.name,
+            TagGroup.EXIF.DateTimeOriginal.name,
+            
+            // GPS Info
+            TagGroup.EXIF.GPSLatitude.name,
+            TagGroup.Composite.GPSLatitude.name,
+            TagGroup.EXIF.GPSLongitude.name,
+            TagGroup.Composite.GPSLongitude.name,
+            TagGroup.EXIF.GPSStatus.name,
+            
+            // Existing Location info
+            TagGroup.IPTC.CountryPrimaryLocationName.name,
+            TagGroup.IPTC.ProvinceState.name,
+            TagGroup.IPTC.City.name,
+            TagGroup.IPTC.Sublocation.name
+        ]
+        
+        try reader.read(tags, from: fullContents)
     }
     
     public func writeImages() throws {
